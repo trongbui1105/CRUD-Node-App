cab2784e26487e606c7536ad0802d1ed312dc5fb
trongbui1105 <buiquoctrong110500@gmail.com>
2021-12-24 08:46:35 +0700

    CRUD App

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
+const path = require('path');
 
 const app = express();
 
@@ -16,9 +17,17 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 // set view engine
 app.set("view engine", "ejs");
+// app.set('views', path.resolve(__dirname, "views/ejs"));
+
+//load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+
 
 app.get("/", (req, res) => {
-  res.send("CRUD application");
+  res.render('index');
 });
 
 app.listen(PORT, () => {
